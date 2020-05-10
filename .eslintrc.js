1c9b020f3c8a8acb94fb62cfe212c699a8c08ba3
Bobby Wang <bobbyquennell@gmail.com>
2020-05-10 18:47:30 +1000

    basic eslint config for Typescript projects
    - add .eslintrc.js for basic TypeScript linting
    - add gitignore file
    - add eslint as a peer dependency
    - to support TypeScript linting on ESlint, add 2 packages:
       @typescript-eslint/eslint-plugin and @typescript-eslint/parser

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+const ERROR = 2;
+const basicConfig = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    semi: [ERROR, 'always'],
+  },
+  globals: {
+    MyGlobal: true,
+  },
+};
+module.exports = basicConfig;
