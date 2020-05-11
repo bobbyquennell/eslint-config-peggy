e1a39f61e808f11a1652f922a2a9097000531529
Bobby Wang <bobbyquennell@gmail.com>
2020-05-11 22:53:24 +1000

    Add eslint support for React

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,26 @@ const basicConfig = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports,
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    }
   },
+  settings: {
+    react: {
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of ReactJS
+    }
+  },
+  // plugins vs extends: https://stackoverflow.com/questions/53189200/whats-the-difference-between-plugins-and-extends-in-eslint
   plugins: [
     '@typescript-eslint',
+    'react',
+    'react-hooks'
   ],
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:react-hooks/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
